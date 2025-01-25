b264d7e8df166a856c072520f58c00a5285fb1d1
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2025-01-25 15:43:07 +0900

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// REST API
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World.";
+	}
+}
