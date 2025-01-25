f31dcff6bec5e71d573a3660dd8fd5513e20c6cc
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2025-01-25 16:05:10 +0900

    create helloworld

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -11,4 +11,9 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World.";
 	}
+
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World.");
+	}
 }
