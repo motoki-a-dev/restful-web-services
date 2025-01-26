8df3600ff5aa225334490734759d80c04fc5cee5
motoki-adachi <mtk157422@gmail.com>
2025-01-26 15:33:06 +0900

    Update UserResource.java

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -33,8 +35,10 @@ public class UserResource {
 	// ユーザーを追加する
 	@PostMapping("/users")
 	public ResponseEntity<User> createUser(@RequestBody User user) {
-		service.save(user);
-		return ResponseEntity.created(null).build();
+		User createdUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(createdUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
