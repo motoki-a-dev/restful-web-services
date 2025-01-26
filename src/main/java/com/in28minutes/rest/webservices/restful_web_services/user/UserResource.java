ee064f356164a4e527576561acfb98912772d5da
motoki-adachi <mtk157422@gmail.com>
2025-01-26 15:27:07 +0900

    create post user

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -2,8 +2,11 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,14 +18,23 @@ public class UserResource {
 		this.service = service;
 	}
 
+	// ユーザーを全権取得する
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
 
+	// 対象IDを持つユーザーを取得する
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+
+	// ユーザーを追加する
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		service.save(user);
+		return ResponseEntity.created(null).build();
 	}
 
 }
